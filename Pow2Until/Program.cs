b7ba15e05ce40fdc46e52fe35370898220936ca3
Simao <simaocampanico.dev@gmail.com>
2025-03-05 15:51:31 +0000

    Adding Pow2Until project solved

---
+++ Pow2Until/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HeroPerk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PowersOf2Until5();
+            PowersOf2Until5();
+        }
+        private static void PowersOf2Until5()
+        {
+            for (int i = 1; i <= (1 << 5); i = i << 1)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
