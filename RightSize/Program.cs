c3e8d980591276aac7037ab664f0171728ae0887
Simao <simaocampanico.dev@gmail.com>
2025-03-05 14:09:21 +0000

    Creating sln and adding the first project

---
+++ RightSize/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RightSize
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
+    }
+}
