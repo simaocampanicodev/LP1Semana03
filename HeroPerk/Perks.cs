465cac0887dcc6f9fdcbc4888bf613d0cc3987cf
Simao <simaocampanico.dev@gmail.com>
2025-03-05 15:06:05 +0000

    Creating HeroPerk project and adding it to the solution

---
+++ HeroPerk/Perks.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace HeroPerks
+{
+    [Flags]
+    enum Perks
+    {
+        WarpShift = 1 << 0,
+        Stealth = 1 << 1,
+        AutoHeal = 1 << 2,
+        DoubleJump = 1 << 3
+    }
+}
---
+++ HeroPerk/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HeroPerk
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
+    }
+}
