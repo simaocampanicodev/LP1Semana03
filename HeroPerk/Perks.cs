8416ec2c6c056d498eb2f0f5117eac07c24db813
txger. <simaocampanico.dev@gmail.com>
2025-03-07 09:55:43 +0000

    Editing some projects.

--- HeroPerk/Perks.cs
+++ HeroPerk/Perks.cs
@@ -5,9 +5,10 @@ namespace HeroPerk
     [Flags]
     enum Perks
     {
-        WarpShift = 1 << 0,
-        Stealth = 1 << 1,
-        AutoHeal = 1 << 2,
-        DoubleJump = 1 << 3
+        None = 0,
+        WarpShift = 1,  
+        Stealth = 2,    
+        AutoHeal = 4,  
+        DoubleJump = 8  
     }
 }
--- HeroPerk/Program.cs
+++ HeroPerk/Program.cs
@@ -1,60 +1,55 @@
 ﻿using System;
-using System.Linq;
 
 namespace HeroPerk
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            if (args.Length == 0)
+            if (args.Length != 1)
             {
-                Console.WriteLine("!No perks at all!");
-                Console.WriteLine("!Not gonna make it!");
+                Console.WriteLine("!Unknown perk!");
                 return;
             }
-
-            Perks perks = 0;
-
-            foreach (char c in args[0])
+            
+            string input = args[0].ToLower();
+            int countW = 0, countS = 0, countA = 0, countD = 0;
+            
+            foreach (char c in input)
             {
                 switch (c)
                 {
-                    case 'w':
-                        perks |= Perks.WarpShift;
-                        break;
-                    case 'a':
-                        perks |= Perks.Stealth;
-                        break;
-                    case 's':
-                        perks |= Perks.AutoHeal;
-                        break;
-                    case 'd':
-                        perks |= Perks.DoubleJump;
-                        break;
+                    case 'w': countW++; break;
+                    case 's': countS++; break;
+                    case 'a': countA++; break;
+                    case 'd': countD++; break;
                     default:
                         Console.WriteLine("!Unknown perk!");
                         return;
                 }
             }
-            if (perks == 0)
+            
+            Perks playerPerks = Perks.None;
+            if (countW % 2 == 1) playerPerks |= Perks.WarpShift;
+            if (countS % 2 == 1) playerPerks |= Perks.Stealth;
+            if (countA % 2 == 1) playerPerks |= Perks.AutoHeal;
+            if (countD % 2 == 1) playerPerks |= Perks.DoubleJump;
+            
+            if (playerPerks == Perks.None)
             {
                 Console.WriteLine("!No perks at all!");
                 Console.WriteLine("!Not gonna make it!");
                 return;
             }
-            var activePerks = Enum.GetValues(typeof(Perks))
-                                      .Cast<Perks>()
-                                      .Where(p => perks.HasFlag(p))
-                                      .Select(p => p.ToString())
-                                      .ToArray();
-            Console.WriteLine(string.Join(", ", activePerks));
-
-            if (perks.HasFlag(Perks.Stealth) && perks.HasFlag(Perks.DoubleJump))
+            
+            Console.WriteLine(playerPerks);
+            
+            if ((playerPerks & Perks.Stealth) != 0 && (playerPerks & Perks.DoubleJump) != 0)
             {
                 Console.WriteLine("!Silent jumper!");
             }
-            if (!perks.HasFlag(Perks.AutoHeal))
+            
+            if ((playerPerks & Perks.AutoHeal) == 0)
             {
                 Console.WriteLine("!Not gonna make it!");
             }
