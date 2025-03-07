82604c86ffc0ed8b20f52c01d14342ab5278297f
txger. <simaocampanico.dev@gmail.com>
2025-03-07 18:04:21 +0000

    Adding final ajusts.

---
+++ RoPaSci/GameItem.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GameItem
+{
+    enum GameItem
+    {
+        Rock,
+        Paper,
+        Scissors
+    }
+}
---
+++ RoPaSci/GameStatus.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GameStatus
+{
+    enum GameStatus
+    {
+        Draw,
+        Player1Wins,
+        Player2Wins
+    }
+}
